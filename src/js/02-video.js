10db7b85a34103ab78a28eec24e20a450c9a7c05
Totalinfonet <totalinfonet@gmail.com>
2023-01-06 22:10:01 +0200

    Vimeo DONE

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,36 @@
+import throttle from 'lodash.throttle';
+import Player from '@vimeo/player';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const LOCAL_STORAGE_KEY = 'videoplayer-current-time';
+
+onReload();
+
+player.on('timeupdate', throttle(onPlay, 1000));
+
+function onPlay(timeupdate) {
+  const videoCurrentTime = timeupdate.seconds.toString();
+
+  localStorage.setItem(LOCAL_STORAGE_KEY, videoCurrentTime);
+}
+
+function onReload() {
+  const savedLocalStorageTime = Number(localStorage.getItem(LOCAL_STORAGE_KEY));
+  if (savedLocalStorageTime) {
+    console.log(savedLocalStorageTime);
+    player
+      .setCurrentTime(savedLocalStorageTime)
+      .then(function () {})
+      .catch(function (error) {
+        switch (error.name) {
+          case 'RangeError':
+            break;
+
+          default:
+            break;
+        }
+      });
+  }
+}
